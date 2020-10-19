71f8772ec90f0db937d17005a6749e53667f572c
Anton Kamenetskiy <lemaxone@gmail.com>
2020-10-19 17:56:08 +0300

    feat: create brackets function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,44 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+ module.exports = function check(str, bracketsConfig) {
+    let brString = '';
+    let brArr = [];
+    let brIndex = 0;
+    let shift = 0;
+    let shift1 = 0;
+    let temp = 0;
+    const num = '8';
+
+    if (str.length % 2 !== 0) return false;
+    for ( let i = 0; i < bracketsConfig.length; i++) {
+        brString += bracketsConfig[i][0] + bracketsConfig[i][1];
+    }
+
+    for (let br of str) {
+        brIndex = brString.indexOf(br);
+        temp = brIndex;
+
+        if ((brString[temp] === brString[temp + 1]) && brString[temp] !== num && shift) {
+            brIndex++;
+            shift = 0;
+        }
+        
+        if ((brString[temp] === brString[temp + 1]) && brString[temp] === num && shift1) {
+            brIndex++;
+            shift1 = 0;
+        }
+
+        if (brIndex % 2 === 0) {
+            brArr.push(brIndex + 1);
+            if ((brString[temp] === brString[temp + 1]) && brString[temp] !== num) {
+                shift = 1;
+            }
+
+            if ((brString[temp] === brString[temp + 1]) && brString[temp] === num) {
+                shift1 = 1;
+            }
+        }
+        else if (brArr.pop() !== brIndex) {
+            return false;
+        }
+    }
+    return true;
 }
